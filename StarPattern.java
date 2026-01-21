90a9fa594949f9bd5a617a287430b9d4b602647d
jay-fibi <jay.gohil@fibilabs.tech>
2026-01-21 21:11:50 +0530

    Add StarPattern.java with multiple star patterns (pyramid, triangle, diamond)

---
+++ StarPattern.java
@@ -0,0 +1,59 @@
+public class StarPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        System.out.println("Pyramid Star Pattern:");
+        System.out.println();
+        
+        // Pyramid pattern
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        System.out.println();
+        System.out.println("Right Triangle Star Pattern:");
+        System.out.println();
+        
+        // Right triangle pattern
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println();
+        System.out.println("Diamond Star Pattern:");
+        System.out.println();
+        
+        // Upper half of diamond
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        // Lower half of diamond
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
